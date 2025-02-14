241d44f8d2116da2ca99b0a95646ecfbf4399e34
Ameen Khan <mak_24x7@hotmail.co.uk>
2025-02-13 20:06:31 -0500

    more import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import Card from './card';
+import Card from './components/card';
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(true);
