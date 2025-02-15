20d565cd2b73b99ea0db225c1f10a64a90fd5996
Ameen <mak_24x7@hotmail.co.uk>
2025-02-15 17:27:08 -0500

    Data fetching and caching (#4)

--- src/app/about/projects/page.tsx
+++ src/app/about/projects/page.tsx
@@ -1,7 +1,34 @@
-export default function AboutProjectsPage() {
+// don't use cache for this component
+// export const dynamic = 'force-dynamic';
+
+export default async function AboutProjectsPage() {
+    // no cache
+    // const response = await fetch('http://localhost:3001/repos', { cache: "no-store" });
+
+    // ttl in cache
+    const response = await fetch('http://localhost:3001/repos', { next: { revalidate: 3 } });
+
+    // default settings
+    // const response = await fetch('http://localhost:3001/repos');
+
+    const repos = await response.json();
+
     return (
         <>
-            Projects!
+            <div className="p-20">
+                <h1 className="mb-8 text-xl">Projects</h1>
+
+                <ul>
+                    {repos.map((repo) =>
+                        <li key={repo.id} className="mb-4">
+                            <div>{repo.title}</div>
+                            <div>{repo.description}</div>
+                            <div>{repo.stargazers_count}</div>
+                        </li>
+                    )}
+                </ul>
+
+            </div>
         </>
     )
 }
