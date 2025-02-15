b47ce3599e66604d903b2571ea148e2a218b80dd
Ameen Khan <mak_24x7@hotmail.co.uk>
2025-02-15 17:26:50 -0500

    diff style of cachin

--- src/app/about/projects/page.tsx
+++ src/app/about/projects/page.tsx
@@ -1,5 +1,16 @@
+// don't use cache for this component
+// export const dynamic = 'force-dynamic';
+
 export default async function AboutProjectsPage() {
-    const response = await fetch('http://localhost:3001/repos');
+    // no cache
+    // const response = await fetch('http://localhost:3001/repos', { cache: "no-store" });
+
+    // ttl in cache
+    const response = await fetch('http://localhost:3001/repos', { next: { revalidate: 3 } });
+
+    // default settings
+    // const response = await fetch('http://localhost:3001/repos');
+
     const repos = await response.json();
 
     return (
