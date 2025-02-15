20145f3dd57a34ef758f470eff548a33677e4684
Ameen <mak_24x7@hotmail.co.uk>
2025-02-15 16:59:20 -0500

    Nextjs basics (#3)

---
+++ src/app/about/projects/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutProjectsPage() {
+    return (
+        <>
+            Projects!
+        </>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import { useState } from 'react';
 import Card from '@/app/components/card';
 
@@ -6,11 +7,13 @@ export default function Home() {
   const [isVisible, setIsVisible] = useState(true);
   const [names, setNames] = useState(['Ameen', 'John', 'Doe']);
 
-  const handleClick = (e) => {
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e);
     setIsVisible(!isVisible);
   }
 
-  const handleAdd = (e) => {
+  const handleAdd = (e: React.MouseEvent<HTMLButtonElement>) => {
+    console.log(e);
     setNames([...names, 'New element!']);
   }
 
