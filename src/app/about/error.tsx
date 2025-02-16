80d37bbd99a2d40f42dc71f2ca33a5ed51f5481f
Ameen Khan <mak_24x7@hotmail.co.uk>
2025-02-16 12:14:31 -0500

    re-render

--- src/app/about/error.tsx
+++ src/app/about/error.tsx
@@ -1,6 +1,8 @@
 'use client' // Error boundaries must be Client Components
 
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { startTransition } from 'react';
 
 export default function Error({
   error,
@@ -14,13 +16,20 @@ export default function Error({
     console.error(error)
   }, [error])
 
+  const router = useRouter();
+
   return (
     <div>
       <h2>Something went wrong!</h2>
       <button
         onClick={
           // Attempt to recover by trying to re-render the segment
-          () => reset()
+          () => {
+            startTransition(() => {
+              router.refresh()
+              reset()
+            })
+          }
         }
       >
         Try again
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,7 @@
 export default function AboutPage() {
-  // throw new Error('oops');
+  if (Math.random() > 0.5) {
+    throw new Error('oops');
+  }
   return (
     <>
       About me!
