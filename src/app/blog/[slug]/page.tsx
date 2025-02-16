50ee6521e3a5d2841dffb81527aa5bb2d0c97a3e
Ameen Khan <mak_24x7@hotmail.co.uk>
2025-02-16 12:19:47 -0500

    add notfound page

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,16 @@
+import { notFound } from "next/navigation";
+
 export default async function BlogPage({
   params,
 }: {
   params: Promise<{ slug: string }>
 }) {
   const slug = (await params).slug;
+
+  if (['first', 'second'].includes(slug)) {
+    notFound();
+  }
+
   return (
     <>
       Hello! {slug}
