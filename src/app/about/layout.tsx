3372629cccc98dd987c907c0cc01335de55b282f
Ameen Khan <mak_24x7@hotmail.co.uk>
2025-02-16 11:34:05 -0500

    add nested layout for about dir

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -1,7 +1,17 @@
 export default function AboutLayout({ children }) {
   return (
     <>
-      {children}
+      <div>
+        {children}
+      </div>
+      <div className="mt-8">
+        <h2 className="mb-4 text-xl">You might also like</h2>
+
+        <ul>
+          <li>First blog post</li>
+          <li>Second blog post</li>
+        </ul>
+      </div>
     </>
   )
 
